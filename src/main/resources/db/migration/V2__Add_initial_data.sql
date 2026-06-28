98e4a14d9fb1e92871a68dc1d448e76ca44dd45a
hong le <vuanhde@yahoo.de>
2026-06-28 23:13:47 +0200

    3.update

--- src/main/resources/db/migration/V2__Add_initial_data.sql
+++ src/main/resources/db/migration/V2__Add_initial_data.sql
@@ -1,18 +1,20 @@
 INSERT INTO
     product (product_uuid, prio, title, descript, searchkeys, imagename, category, masstype, price)
 VALUES
-    ('510a0d7e-8e83-4193-b483-e27e09ddc34d', 2, 'Mastering Java Persistence', 'co so du lieu', 'mastering java persistence api', 'book-one.jpg', 'book', 'hose', 19.99),
-    ('03fef6ac-1896-4ce8-bd69-b798f85c6e0b', 2, 'JAVA Programming Simplified', 'lap trinh java', 'java programming simplified', 'book-two.jpg', 'book', 'hose', 29.99),
-    ('d3588630-ad8e-49df-bbd7-3167f7efb246', 2, 'Software Design Patterns', 'thiet ke phan mem', 'software design patterns for java developers', 'book-three.jpg', 'book', 'hose', 39.99),
-    ('510a0d7e-8e83-4193-b594-e27e09ddc45d', 2, 'Hanoi Street food', 'am thuc pho phuong', 'hanoi streetfood food hanoi city', 'book-one.jpg', 'book', 'hose', 29.99),
-    ('510a0d7e-8e83-4193-b605-e27e09ddc56d', 2, 'Something funny', 'chuyen vui ngay nay', 'something funny things ', 'book-one.jpg', 'book', 'hose', 9.99),
-    ('892422cd-0835-479e-84de-9265d3f2dc6a', 2, 'Dell Laptop', 'may tinh de ban', 'dell laptop', 'dell-computer.jpg', 'laptop', 'hemd', 49.99),
-    ('9d927bff-8d5b-4929-8c26-a9be00173adc', 2, 'Samsung A350 Handy', 'dien thoai cam tay', 'samsung a350 handy', 'sam-sung-pjone.jpg', 'handy', 'hemd', 59.99),
+    ('510a0d7e-8e83-4193-b483-e27e09ddc34d', 2, 'Mastering Java Persistence', 'co so du lieu', 'mastering java persistence api', 'book-one.jpg;body-masse.jpg', 'book', 'hose', 19.99),
+    ('03fef6ac-1896-4ce8-bd69-b798f85c6e0b', 2, 'JAVA Programming Simplified', 'lap trinh java', 'java programming simplified', 'book-two.jpg;body-masse.jpg', 'book', 'hose', 29.99),
+    ('d3588630-ad8e-49df-bbd7-3167f7efb246', 2, 'Software Design Patterns', 'thiet ke phan mem', 'software design patterns for java developers', 'book-three.jpg;body-masse.jpg', 'book', 'hose', 39.99),
+    ('510a0d7e-8e83-4193-b594-e27e09ddc45d', 2, 'Hanoi Street food', 'am thuc pho phuong', 'hanoi streetfood food hanoi city', 'book-one.jpg;body-masse.jpg', 'book', 'hose', 29.99),
+    ('510a0d7e-8e83-4193-b605-e27e09ddc56d', 2, 'Something funny', 'chuyen vui ngay nay', 'something funny things ', 'book-one.jpg;body-masse.jpg', 'book', 'hose', 9.99),
+    ('892422cd-0835-479e-84de-9265d3f2dc6a', 2, 'Dell Laptop', 'may tinh de ban', 'dell laptop', 'dell-computer.jpg;body-masse.jpg', 'laptop', 'hemd', 49.99),
+    ('9d927bff-8d5b-4929-8c26-a9be00173adc', 2, 'Samsung A350 Handy', 'dien thoai cam tay', 'samsung a350 handy', 'sam-sung-pjone.jpg;body-masse.jpg', 'handy', 'hemd', 59.99),
 
-    ('510a0d7e-8183-4193-b594-e27e09dhc45e', 1, 'Hanoi Pijama', 'quan thung kieu ha noi', 'hanoi pijama quan ngu quan ba ba quan thung', 'quan-pijama.jpg', 'damen-hose', 'hose', 29.99),
-    ('510a0d7e-8183-4193-b594-e27e09ddc45e', 1, 'Woman TShirt', 'ao phong nu', 'woman shirt ao thun nu ao thun', 'ao-thun-nu.jpg', 'damen-hemd', 'hemd', 29.99),
-    ('510a0d7e-8483-4194-b605-e29e09ddc56f', 1, 'City Jean', 'quan bo thanh pho', 'city jean quan gin quan jean quan bo', 'quan-jean.jpg', 'herren-hose', 'hose', 19.99),
-    ('510a0d7e-8483-4193-b605-e27e09ddc56f', 1, 'Saigon TShirt', 'ao phong sai gon', 'saigon tshirt ao thun ao phong', 'ao-thun.jpg', 'herren-hemd', 'hemd', 9.99); 
+    ('510a0d7e-8183-4193-b594-e27e09dhc45e', 1, 'Hanoi Pijama', 'quan thung kieu ha noi quan thung kieu ha noi quan thung kieu ha noi quan thung kieu ha noi quan thung kieu ha noi quan thung kieu ha noi', 'hanoi pijama quan ngu quan ba ba quan thung', 'quan-pijama.jpg;woman_mass.jpg', 'damen-hose', 'hose', 29.99),
+    ('510a0d6e-8183-4193-b584-e27e09dhc45e', 1, 'Kurzes Kleid', 'vay ngan vay nu vay mua he kurzes kleid', 'vay ngan vay nu vay mua he kurzes kleid', 'vay-ngan.jpg;woman_mass.jpg', 'damen-hose', 'hose', 39.99),
+    ('510a0d6e-8284-4193-b584-e27e09dhc45d', 1, 'Mini Trägerkleid', 'mini trägerkleid mini träger kleid sommer kleid vay ngan vay nu vay mua he kurzes kleid', 'mini trägerkleid mini träger kleid sommer kleid vay ngan vay nu vay mua he kurzes kleid', 'vay-he26.jpg;vay-he26b.jpg', 'damen-hose', 'hose', 19.99),
+    ('510a0d7e-8183-4193-b594-e27e09ddc45e', 1, 'Woman TShirt', 'ao phong nu', 'woman shirt ao thun nu ao thun', 'beautygirl.jpg;woman_top.jpg', 'damen-hemd', 'hemd', 29.99),
+    ('510a0d7e-8483-4194-b605-e29e09ddc56f', 1, 'City Jean', 'quan bo thanh pho', 'city jean quan gin quan jean quan bo', 'jean-boy26.jpg;man_bottom.jpg', 'herren-hose', 'hose', 19.99),
+    ('510a0d7e-8483-4193-b605-e27e09ddc56f', 1, 'Saigon TShirt', 'ao phong sai gon', 'saigon tshirt ao thun ao phong', 'tshirt-saigonb.jpg;tshirt-saigon.jpg', 'herren-hemd', 'hemd', 9.99); 
 
 
 -- INSERT INTO
